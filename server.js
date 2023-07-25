2a25657c4901cf0b1c96cd9c21fb9572a7976b20
lee <ljyeon0713@naver.com>
2023-07-25 18:03:57 +0900

    feat: 채팅 기능 구현중

--- server.js
+++ server.js
@@ -1,54 +1,49 @@
-const express = require("express");
-var cors = require("cors");
-const http = require("http");
+const express = require('express');
+var cors = require('cors');
+const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const io = require('socket.io')(server);
 
 app.use(cors());
-app.set("port", process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000);
 // app.get("/", (req, res) => {
 //     res.send({ data: "hi" });
 // });
 
-const openedRoomList = [];
+const openedRoomList = {};
 
-app.post("/create-room", (req, res) => {
-    const roomNumber = Math.floor(Math.random() * 100000);
-    const id = String(Math.floor(Math.random() * 10000000));
-    openedRoomList.push({
-        number: roomNumber,
-        users: [{ id, nickname: "" }],
-    });
-    res.send({ id, roomNumber, success: true });
+app.post('/create-room', (req, res) => {
+  const roomNumber = Math.floor(Math.random() * 100000);
+  // const id = String(Math.floor(Math.random() * 10000000));
+  openedRoomList.roomNumber = {
+    users: {},
+  };
+  // openedRoomList.roomNumber.users[id] = { nickname: '' };
+  res.send({ roomNumber, success: true });
 });
 
-io.on("connection", function (socket) {
-    if (openedRoomList[0]) console.log(openedRoomList[0].users);
-    socket.on("joinRoom", async (roomNumber) => {
-        socket.join(roomNumber);
-        let id = String(Math.floor(Math.random() * 10000000));
-        io.to(roomNumber).emit("joinSuccess", id);
-        socket.on("setNickname", (id, nickname) => {
-            openedRoomList
-                .filter((room) => room.number == roomNumber)[0]
-                .users.push({
-                    id,
-                    nickname,
-                });
-            console.log(openedRoomList[0].users);
-        });
-        socket.on("chat", (chat) => {
-            console.log(chat);
-            io.to(roomNumber).emit("chat", chat);
-        });
-        socket.on("disconnect", () => {
-            socket.leave(roomNumber);
-            console.log("disconnected");
-        });
+io.on('connection', function (socket) {
+  socket.on('joinRoom', async (roomNumber) => {
+    socket.join(roomNumber);
+    let id = String(Math.floor(Math.random() * 10000000));
+    openedRoomList.roomNumber.users[id] = id;
+    io.to(roomNumber).emit('joinSuccess', id);
+    socket.on('setNickname', (id, nickname) => {
+      openedRoomList.roomNumber.users[id] = {
+        nickname: nickname,
+      };
     });
+    socket.on('chat', (id, chat) => {
+      console.log(id, chat);
+      io.to(roomNumber).emit('chat', id, chat);
+    });
+    socket.on('disconnect', () => {
+      socket.leave(roomNumber);
+    });
+  });
 });
 
-server.listen(app.get("port"), () => {
-    console.log("isRunning on 5000");
+server.listen(app.get('port'), () => {
+  console.log('isRunning on 5000');
 });
